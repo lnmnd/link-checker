2b80d73ec12b913d4ec344b30de9fd286ab962a9
lnmnd <lnmnd@users.noreply.github.com>
2016-07-15 16:42:15 +0200

    Use argparse

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import logging
 import signal
 import pykka.debug
@@ -13,18 +13,20 @@ def end_callback():
 
 
 if __name__ == "__main__":
-    if not len(sys.argv) == 2:
-        print("usage: app.py url")
-        sys.exit()
-    url = sys.argv[1]
-    timeout = 3
-    rate = 10
-    user_agent = "LinkChecker/0.1"
-    create_timer = lambda parent: checker.Timer.start(parent, timeout).proxy()
-    create_pulse = lambda parent: checker.Pulse.start(parent, 1 / rate).proxy()
-    create_fetcher = lambda parent: (checker.Fetcher.start(parent, user_agent)
-                                     .proxy())
-    (checker.Checker.start(url, end_callback,
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", type=str)
+    parser.add_argument("--timeout", type=int, default=3)
+    parser.add_argument("--rate", type=float, default=10)
+    parser.add_argument("--useragent", type=str, default="LinkChecker/0.1")
+    args = parser.parse_args()
+
+    create_timer = (lambda parent:
+                    checker.Timer.start(parent, args.timeout).proxy())
+    create_pulse = (lambda parent:
+                    checker.Pulse.start(parent, 1 / args.rate).proxy())
+    create_fetcher = (lambda parent:
+                      checker.Fetcher.start(parent, args.useragent).proxy())
+    (checker.Checker.start(args.url, end_callback,
                            create_timer, create_pulse, create_fetcher)
      .proxy()
      .run())
